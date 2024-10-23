b4380672c5f773c084106554b6ef7cc9941d1ce4
Sachaa-Thanasius <i.like.ike101@gmail.com>
2024-10-23 16:48:06 +0530

    Bump version.

--- src/defer_imports/__init__.py
+++ src/defer_imports/__init__.py
@@ -16,7 +16,7 @@ import zipimport
 from importlib.machinery import BYTECODE_SUFFIXES, SOURCE_SUFFIXES, FileFinder, ModuleSpec, PathFinder, SourceFileLoader
 
 
-__version__ = "0.1.0.dev1"
+__version__ = "0.1.0"
 
 __all__ = (
     "install_import_hook",
