632db8b23006282a7f8a0a9fb869ff2780399a82
Sachaa-Thanasius <i.like.ike101@gmail.com>
2025-04-30 23:12:38 -0400

    Add rough scripts used for some profiling.

---
+++ bench/profile_sample_local.py
@@ -0,0 +1,15 @@
+import sys
+
+import defer_imports.ast_rewrite
+
+
+def profile_defer_imports_local() -> None:
+    for _ in range(100):
+        with defer_imports.ast_rewrite.import_hook(uninstall_after=True):
+            import bench.sample_defer_local  # pyright: ignore [reportUnusedImport]
+
+        del sys.modules["bench.sample_defer_local"]
+
+
+if __name__ == "__main__":
+    profile_defer_imports_local()
